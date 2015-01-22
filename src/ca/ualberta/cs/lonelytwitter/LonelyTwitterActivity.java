e39fb932adbdea62103a8ed59f64e36643ab71c5
Ben Hunter <bkhunter@ualberta.ca>
2015-01-22 16:32:31 -0700

    Lonely Twitter for lab 3. Added Gson File IO.

--- src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -6,12 +6,18 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Date;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+//import com.google.gson.reflect.TypeToken;
 
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.renderscript.Type;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -23,6 +29,8 @@ public class LonelyTwitterActivity extends Activity {
 	private static final String FILENAME = "file.sav";
 	private EditText bodyText;
 	private ListView oldTweetsList;
+	private ArrayList<String> tweets;
+	private ArrayAdapter<String> adapter;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -39,8 +47,11 @@ public class LonelyTwitterActivity extends Activity {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
 				String text = bodyText.getText().toString();
+				tweets.add(text);
+				adapter.notifyDataSetChanged();
 				saveInFile(text, new Date(System.currentTimeMillis()));
-				finish();
+				//finish();
+				//throw new RuntimeException("test error");
 
 			}
 		});
@@ -49,6 +60,8 @@ public class LonelyTwitterActivity extends Activity {
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
+		
+		super.onStart();
 		
 		User u = new Author(); // uses the default constructor to create a new object
 		Friendship f = new Friend();
@@ -65,23 +78,31 @@ public class LonelyTwitterActivity extends Activity {
 			
 		}
 		
-		super.onStart();
-		String[] tweets = loadFromFile();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+		tweets = loadFromFile();
+		adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
 	}
 
-	private String[] loadFromFile() {
+	private ArrayList<String> loadFromFile() {
+		Gson gson = new Gson();
 		ArrayList<String> tweets = new ArrayList<String>();
 		try {
 			FileInputStream fis = openFileInput(FILENAME);
-			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
-			String line = in.readLine();
-			while (line != null) {
-				tweets.add(line);
-				line = in.readLine();
-			}
+			//taken from http://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html on 01/22/2015
+			
+			java.lang.reflect.Type ListType = new TypeToken<ArrayList<String>>() {}.getType();
+			InputStreamReader isr = new InputStreamReader(fis);
+			
+			tweets = gson.fromJson(isr,ListType);
+			fis.close();
+			
+//			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+//			String line = in.readLine();
+//			while (line != null) {
+//				tweets.add(line);
+//				line = in.readLine();
+//			}
 
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -90,15 +111,24 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return tweets.toArray(new String[tweets.size()]);
+		if (tweets == null) {
+			tweets = new ArrayList<String>();
+		}
+		return tweets;
+				//tweets.toArray(new String[tweets.size()]);
 	}
 	
 	private void saveInFile(String text, Date date) {
+		Gson gson = new Gson();
+		
 		try {
 			FileOutputStream fos = openFileOutput(FILENAME,
-					Context.MODE_APPEND);
-			fos.write(new String(date.toString() + " | " + text)
-					.getBytes());
+					0); 	//this is the default mode --> will lose data --> replaced Context.MODE_APPEND);
+			
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			gson.toJson(tweets, osw); //replaces fos.write(new String(date.toString() + " | " + text)
+							 		  //		.getBytes());
+			osw.flush();
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
